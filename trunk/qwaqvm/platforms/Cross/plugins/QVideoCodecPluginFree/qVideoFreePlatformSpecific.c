01742cd468fd26b5be64f1e84b0bda67bbd90d40
schwaj@gmail.com <schwaj@gmail.com@006d1119-9e84-9df8-f51b-87c3da03f371>
2011-05-31 07:22:20 +0000

    Work though a bunch of annoying bugs to finally build a plugin that links with libav* and libx264.

--- trunk/qwaqvm/platforms/Cross/plugins/QVideoCodecPluginFree/qVideoFreePlatformSpecific.c
+++ trunk/qwaqvm/platforms/Cross/plugins/QVideoCodecPluginFree/qVideoFreePlatformSpecific.c
@@ -18,10 +18,12 @@
 #include "qVideoCodecPlatformSpecific.h"
 #include "qVideoCommon.h"
 
+#include "avformat.h"
+
 /* Declared in ../QVideoCodecPlugin/qVideoCodecPlatformSpecific.h */
 void qInitPlatform(void)
 {
-	/* nothing needs to be done */
+	av_register_all();
 }
 
 /* Declared in ../QVideoCodecPlugin/qVideoCodecPlatformSpecific.h */
@@ -29,5 +31,17 @@ void qShutdownPlatform(void)
 {
 	/* nothing needs to be done */
 }
- 
 
+
+/* The internet gives conflicting info about this.  According to some sources, it exists
+   on OS X, and indeed if I type "man posix_memalign" then it shows up (but I still can't
+   link).  Others say that the OS X ABI is 16-byte aligned, and so posix_memalign(); they
+   also say that it's safe to define your own that simply calls malloc().
+*/
+#if defined __APPLE__
+#include <memory.h>
+void *posix_memalign(unsigned int size)
+{
+    return malloc(size);
+}
+#endif 
